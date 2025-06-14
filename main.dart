9b62a945e3ec2cdc18ec7d06e2af81a752dded24
Rian Alves <rianalvesleal@gmail.com>
2025-06-14 11:47:09 -0300

    Inserido comentários e modificação da função principal 'executar_estrategia'

--- main.dart
+++ main.dart
@@ -6,24 +6,25 @@ void ordenacaoDecrescente(List<int> lista){
     print(lista);
 }
 
-void executar_estrategia(List<int> lista, var estrategia){
-    if(estrategia==1){
-        ordenacaoDecrescente(lista);
-    }
-}
-
 List<int> gerarLista(){
-    List<int> lista = List.filled(10, 0);
+    List<int> lista = List.filled(10, 0);   //Inicializa a lista com 0
     for(var i=0; i<10; i++){
-        lista[i] = Random().nextInt(100);
+        lista[i] = Random().nextInt(100);   //Insere um número aleatório entre 0 a 100 na lista a cada iteração
     }
     return lista;
 }
 
-void main(){
-    var estrategia = 1;
-    List<int> lista = gerarLista();
+void executar_estrategia(void Function(List<int>) estrategia){  //Esse parâmetro mostra o retorno e o parâmetro para a função passada
+    List<int> lista = gerarLista();     //função para criar uma lista aleatória
     print("Lista aleatória gerada:");
     print(lista);
-    executar_estrategia(lista, estrategia);
+    //chamada da função passada por parâmetro
+    estrategia(lista);
+}
+
+void main(){
+    executar_estrategia(ordenacaoDecrescente);
+    print()
+    //Chamar a mesma função passando por parâmetro as outras funções...
+    //coloque um print() depois de chamar a função para ter um espaço após a imprimir a lista 
 }
